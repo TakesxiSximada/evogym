b1c47213746f81e5092625aed4e9d348e4eb61aa
TakesxiSximada <sximada@gmail.com>
2023-04-08 11:11:29 +0900

    fix tests

---
+++ tests/test_render.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+import gym
+import evogym.envs
+from evogym import sample_robot
+
+class RenderTest(TestCase):
+    def test_it(self):
+        body, connections = sample_robot((5,5))
+        env = gym.make('Walker-v0', body=body)
+        env.reset()
+        
+        for _ in range(100):
+            action = env.action_space.sample()-1
+            ob, reward, done, info = env.step(action)
+            env.render() 
+        
+            if done:
+                env.reset()
+        
+        env.close()        
